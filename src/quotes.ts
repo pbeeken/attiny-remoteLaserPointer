e067a450c0f2efd338e68fd201f8640b3a0c55bc
P. Beeken <pbeeken@byramhills.org>
2022-07-26 19:16:55 -0400

    Many explorations on how to connect.

--- src/quotes.ts
+++ src/quotes.ts
@@ -1,9 +1,15 @@
-// Load HTTP module
-//const http = require("http")
+//# sourceMappingURL=quotes.js.map
+// Load modules
 import * as http from "http"
 import * as fs from "fs/promises"
+import * as net from "net"
 
-let quotes = []
+const hostname: string = "10.110.110.156" //"127.0.0.1"
+const port: number = 8000
+const endl: string = "\n\r"
+
+let quotes: string[] = []
+let fileList: any = ""
 
 /**
  * read the quotes into memory
@@ -12,7 +18,7 @@ let quotes = []
  * With live updating by changing the contents of the quotes and see how it manifests in the client page.
  * @param fn is the server filename containing quotes
  */
-async function readQuotes(fn) {
+async function readQuotes(fn: string) {
     try {
         const data = await fs.readFile(fn, {encoding:'utf8'})
         quotes = data.split('\n')
@@ -20,6 +26,22 @@ async function readQuotes(fn) {
         console.log(err)
     }
 }
+
+let script: string = ""
+/**
+ * Read script file
+ */
+async function readScriptFile(fn: string) {
+    try {
+        console.log(`Reading...${fn}`)
+        const data = await fs.readFile(fn, {encoding:'utf8'})
+        script = data
+        console.log(`${script.length}`)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 
 /**
  * From the file, pick a line randomly and providing it for displays
@@ -36,34 +58,102 @@ function picRandomQuote() {
     return quotes[i]
 }
 
-const hostname = "10.110.110.156" //"127.0.0.1"
-const port = 8000
+async function getFileList() {
+    try {
+        const data = await fs.readdir(".") // list of files
+        fileList = data.join("<br/>")
+    } catch(err) {
+        console.log(err)
+    }
+}
 
+getFileList()
 
 /** Create HTTP server
- *  
+ *  Main program, essentially
  **/ 
 const server = http.createServer( (req, res) => {
+    console.log('---------------------------------------------------------------')
 
-    // Set the response HTTP header with HTTP status and Content type
-    res.writeHead(200, { 'Content-Type': 'text/html' })
+    if(req.url === "/index.html") {
+        // Set the response HTTP header with HTTP status and Content type
+        res.writeHead(200, { 'Content-Type': 'text/html' })
 
-    res.write('<html>')
-    res.write('<head>')
-    res.write('<meta http-equiv="refresh" content="10">')
-    res.write('<meta http-Equiv="Cache-Control" Content="no-cache" />')
-    res.write('</head>')
+        res.write('<html>'+endl)
+        res.write('<head>'+endl)
+        res.write('<meta http-equiv="refresh" content="10">'+endl)
+//        res.write('<meta http-Equiv="Cache-Control" Content="no-cache"/>'+endl)
+//        res.write('<script src="client.js"></script>'+endl)
+        // readScriptFile('client.js') //.then((contents) =>{
+        //     res.write('<script>'+endl)
+        //     res.write(script)
+        //     res.write('</script>'+endl)
+         //   })
 
-    res.write('<body>')
-    res.write('<h1>Hello World</h1>')
-    res.write('<p>This is some tricky up stuff!</p>')
-    res.write(`<b>${picRandomQuote()}</b>`)
-    res.write('</body')
+        res.write('</head>'+endl)
 
-    res.end('</html>')
+        res.write('<body>'+endl)
+        res.write('<h1>Hello World</h1>'+endl)
+        res.write('<p>This is some tricky up stuff!</p>'+endl)
+        res.write(`<b>${picRandomQuote()}</b>`+endl)
+
+        res.write('<br/>'+endl+'<br/>'+endl+'<br/>'+endl)
+
+
+        res.write('Files:<br/>'+endl)
+    //    res.write(`${fileList.length}<br/>`+endl)
+        res.write(`${fileList}`+endl)
+
+
+        res.write('</body>'+endl)
+
+        res.end('</html>'+endl)
+        console.log(`${req.url} handled.`)
+    }
+
+    else if (req.url === "/client.js") {
+        let data = ""
+        readScriptFile('client.js').then((contents) =>{
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end(contents)
+            })
+        console.log(`${req.url} handled.`)
+    }
+
+    else if (req.url.endsWith(".map")) {
+        const mapFile = (req.url).substring(1)
+        let data = ""
+        readScriptFile(mapFile).then((contents) =>{
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end(contents)
+        })
+        console.log(`${req.url} handled.`)
+    }
+    else
+        console.log(`${req.url} NOT handled.`)
+
+
 })
 
 // Prints a log once the server starts listening
-server.listen(port, hostname, function() {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`)
 })
+
+
+// Socket Server
+// const sockServ = net.createServer((c) => {
+//   // 'connection' listener.
+//   console.log('client connected');
+//   c.on('end', () => {
+//     console.log('client disconnected');
+//   });
+//   c.write('hello\r\n');
+//   c.pipe(c);
+// });
+// sockServ.on('error', (err) => {
+//   throw err;
+// });
+// sockServ.listen(8124, () => {
+//   console.log('server bound');
+// });
