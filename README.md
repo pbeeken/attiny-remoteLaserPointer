329b2a9d9ade44c729f3a9e0a2c2df1fc06b83b7
P. Beeken <pbeeken@byramhills.org>
2022-07-27 10:55:45 -0400

    experimenting with REST call parsing

--- README.md
+++ README.md
@@ -6,16 +6,19 @@ the PIO pins and $I^2C$ devices.  This may include output as well as input.
 General things I need to learn:
   - How async operations work with the Promise model of code handling
     - looks like any function declared with an 'async' prefix automatically becomes a promise object.
-      - YES.
+      - *YES.*
     - Promise objects need to be provided functions (or lambda functions) that complete the transaction or respond to various options of what the promise can transfer.
-      - If you are constructing your own manually yes, but you should not need to do that
+      - *If you are constructing your own manually yes, but you should not need to do that*
   - When do I control caching and update content pushed from the server vs being requested by client?
     - If the data is static then cache. If it is fluid then get it on the fly.  The interesting thing with the experiments is that if the returned data isn't querried or operated on it loads more slowly. (or appears to)
-    - If you're getting really detailed you can control caching with an http header, but its not necessary.
+    - *If you're getting really detailed you can control caching with an http header, but its not necessary.*
 
 ## Naive starting point: Quotes
 I have a server file which contains quotes in a text file. I read the file in to a server script and serves pages with a randomly changing choice from the file.  
   - Currently this is driven by the client updating the page using a refresh. **Is there a way the server can push this out?**  
-  - I write all the structural elements directly within the code. **Is this the best way to do this or should the static bits be in a file with the program managing the dynamic elements?**
-    - Jury's still out on this one. Getting information back and forth might be a bit tricky but I am now looking at wrtiting code that does requests through fetch calls.  Push tech is still above my paygrade right now.  I am still thinking a simple socket layer would be OK.
+  - I write all the structural elements directly within the code. **Is this the best way to do this or should the static bits be in a file with the program managing the dynamic elements?** *Yes, Use templates and modify as they are fed through. Consider, if the project gets more complicated, a framework like **koajs** (just an example)*  
+    - Jury's still out on this one. Getting information back and forth might be a bit tricky but I am now looking at wrtiting code that does requests through fetch calls.  Push tech is still above my paygrade right now.  I am still thinking a simple socket layer would be OK. *Use REST calls not the overhead of sockets*
     - [Methods for transferring data](https://www.c-sharpcorner.com/UploadFile/suthish_nair/different-ways-to-pass-data-between-web-forms/) gives a nice summary of options.
+
+## Growth
+Now that the work infrastructure is better developed pay attention to how the `eslinter` and `eslint-prettier` do their job. They will provide internal on the fly documentation. 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,29 +1,59 @@
+/**
+ * This file was made with a lot of help from Neal. The documentation contained within are my notes
+ * as I try to recollect the instructions and guidance from the next morning.
+ */
+
+/**
+ * Imports of libraries and typedefs
+ */
 import { readFile } from 'node:fs/promises';
 import * as http from 'node:http';
+import * as querystring from 'node:querystring';
 import { on } from 'node:events';
 import * as path from 'node:path';
 import type { IncomingMessage, ServerResponse } from 'node:http';
 
+/**
+ * Instead of creating all the operational elements (see quotes.ts) on the fly (lambda functions)
+ * Build incrementally to compartmentalize the features.
+ */
+
+/**
+ * Create a server instance
+ */
 const server = http.createServer();
-server.listen(8000, '0.0.0.0', 128, () =>
+/**
+ * Add the listener (0.0.0.0) provides a universal reception space.
+ */
+server.listen(8080, '0.0.0.0', 128, () =>
   console.log('Listening on http://localhost:8080')
 );
 
+/**
+ * localizes the place where templates and data is stored.
+ */
 const BASE_DIR = 'public';
 
+/**
+ * This is a key idea: provide our primary entry point as an async function so we can
+ * implement await decorators that control what is critical vs what can be queued.
+ */
 async function main() {
-  for await (const [request, response] of on(
-    server,
-    'request'
-  ) as AsyncIterable<[IncomingMessage, ServerResponse]>) {
-    const url = new URL(
-      request.url ?? '/',
-      `http://${String(request.headers.host)}`
-    );
+  /*                                      listen to the 'request' events to get the details for incoming requests */
+  // eslint-disable-next-line prettier/prettier
+  for await (const [request, response] of on(server, 'request') as AsyncIterable<[IncomingMessage, ServerResponse]>) {
+    // eslint-disable-next-line prettier/prettier
+    const theURL = new URL(request.url ?? '/', `http://${String(request.headers.host)}` ); /* parse the url for easier testing below */
 
-    console.log(request.method, url.pathname);
+    console.log(
+      request.method,
+      theURL.pathname,
+      theURL.search
+    ); /* report what we have so far. */
 
     try {
+      /* handle the various request conditions from the connection TODO: review the http communications protocols just for context */
+      // OPTIONS request
       if (request.method === 'OPTIONS') {
         response.writeHead(204, {
           'Access-Control-Allow-Origin': '*',
@@ -31,21 +61,29 @@ async function main() {
           'Access-Control-Max-Age': Number.MAX_SAFE_INTEGER,
         });
         response.end();
-      } else if (url.pathname === '/') {
+
+        // Default page rquest without and explicit request for index.html
+      } else if (theURL.pathname === '/') {
         response.setHeader('content-type', 'text/html');
         response.end(await readFile(path.join(BASE_DIR, 'index.html')), 'utf8');
-      } else if (url.pathname.startsWith('/api')) {
+
+        // marks the start of a REST call
+      } else if (theURL.pathname.startsWith('/api')) {
         // REST call handling
         // browser side code: await fetch('/api').then(r => r.json())
+        const query = querystring.parse(theURL.search);
+        console.log(query);
         response.end(JSON.stringify({ led: 'on' }));
+
+        // grab any file which matches the request
       } else {
         const fileData = await readFile(
-          path.join(BASE_DIR, url.pathname.slice(1))
+          path.join(BASE_DIR, theURL.pathname.slice(1))
         );
 
         // Get the extension
-        const fileParts = url.pathname.split('.');
-        const fileExtension = fileParts[fileParts.length - 1];
+        const fileParts = theURL.pathname.split('.');
+        const fileExtension = fileParts[fileParts.length - 1]; // could also be .slice(-1)
 
         if (fileExtension === 'js') {
           response.setHeader('content-type', 'application/javascript');
@@ -56,8 +94,11 @@ async function main() {
         if (fileExtension === 'map' || fileExtension === 'json') {
           response.setHeader('content-type', 'application/json');
         }
+        if (fileExtension === 'html') {
+          response.setHeader('content-type', 'text/html');
+        }
 
-        response.end(fileData, 'utf8');
+        response.end(fileData, 'utf8'); //send the contents
       }
     } catch (error) {
       response.statusCode = 500;
@@ -67,6 +108,9 @@ async function main() {
   }
 }
 
+/**
+ * Now we run the 'main' promise.
+ */
 main()
   .then(() => console.log('end'))
   .catch((e) => console.log(e))
