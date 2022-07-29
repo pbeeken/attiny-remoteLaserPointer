8bd2fd4aa247e412bf61d902de8f107cdb5dc3a0
P. Beeken <pbeeken@byramhills.org>
2022-07-29 12:33:36 -0400

    Formatting change to trigger autoupdate

--- src/server/auto_update.ts
+++ src/server/auto_update.ts
@@ -3,24 +3,26 @@ import { execSync } from 'child_process';
 let gitPuller = null;
 
 if (process.env.USER === 'pi') {
-  gitPuller = setTimeout(() => {
-    try {
-      execSync('git fetch');
-      const localHash = execSync('git rev-parse HEAD', { encoding: 'utf8' });
-      const remoteHash = execSync(`git rev-parse 'main@{upstream}'`, {
-        encoding: 'utf8',
-      });
+    gitPuller = setTimeout(() => {
+        try {
+            execSync('git fetch');
+            const localHash = execSync('git rev-parse HEAD', {
+                encoding: 'utf8',
+            });
+            const remoteHash = execSync(`git rev-parse 'main@{upstream}'`, {
+                encoding: 'utf8',
+            });
 
-      if (localHash !== remoteHash) {
-        console.log(`localHash=${localHash}, remoteHash=${remoteHash}`);
-        console.log(`Updating code...`);
-        execSync('git pull');
-        console.log(`Updated code`);
-      }
-    } catch {
-      console.log('failed to pull');
-    }
-  }, 60_000);
+            if (localHash !== remoteHash) {
+                console.log(`localHash=${localHash}, remoteHash=${remoteHash}`);
+                console.log(`Updating code...`);
+                execSync('git pull');
+                console.log(`Updated code`);
+            }
+        } catch {
+            console.log('failed to pull');
+        }
+    }, 60_000);
 }
 
 export {};
